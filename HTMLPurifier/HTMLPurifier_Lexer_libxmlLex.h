38ea310a1accb8776d5cab1e0a06be57cac1e454
Roman Priebe <roman.priebe@gmail.com>
2014-01-12 12:28:38 +0100

    HTMLPurifierLexer_libxmlLex added (still empty)

---
+++ HTMLPurifier/HTMLPurifier_Lexer_libxmlLex.h
@@ -0,0 +1,13 @@
+//
+//  HTMLPurifier_Lexer_libxmlLex.h
+//  HTMLPurifier
+//
+//  Created by Roman Priebe on 12.01.14.
+//  Copyright (c) 2014 Mynigma. All rights reserved.
+//
+
+#import "HTMLPurifier_Lexer.h"
+
+@interface HTMLPurifier_Lexer_libxmlLex : HTMLPurifier_Lexer
+
+@end
---
+++ HTMLPurifier/HTMLPurifier_Lexer_libxmlLex.m
@@ -0,0 +1,13 @@
+//
+//  HTMLPurifier_Lexer_libxmlLex.m
+//  HTMLPurifier
+//
+//  Created by Roman Priebe on 12.01.14.
+//  Copyright (c) 2014 Mynigma. All rights reserved.
+//
+
+#import "HTMLPurifier_Lexer_libxmlLex.h"
+
+@implementation HTMLPurifier_Lexer_libxmlLex
+
+@end
