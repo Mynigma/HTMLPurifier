68a6bb2ddcebd2095da1f6d463dd2831c02ec5b5
gitir1 <lksnmnn@gmail.com>
2014-01-12 20:21:45 +0100

    commit

---
+++ HTMLPurifier/HTMLPurifier_AttrDef_HTML_MultiLength.h
@@ -0,0 +1,13 @@
+//
+//  HTMLPurifier_AttrDef_HTML_MultiLength.h
+//  HTMLPurifier
+//
+//  Created by Lukas Neumann on 12.01.14.
+//  Copyright (c) 2014 Mynigma. All rights reserved.
+//
+
+#import "HTMLPurifier_AttrDef_HTML_Length.h"
+
+@interface HTMLPurifier_AttrDef_HTML_MultiLength : HTMLPurifier_AttrDef_HTML_Length
+
+@end
