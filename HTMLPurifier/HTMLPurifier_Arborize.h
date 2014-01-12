e0361d72ec84423bf4a06f3722b86cd47cd7c443
Roman Priebe <roman.priebe@gmail.com>
2014-01-12 22:34:26 +0100

    HTMLPurifier_Definition done

--- HTMLPurifier/HTMLPurifier_Arborize.h
+++ HTMLPurifier/HTMLPurifier_Arborize.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+@class HTMLPurifier_Node, HTMLPurifier_Config, HTMLPurifier_Context;
+
 /**
  * Converts a stream of HTMLPurifier_Token into an HTMLPurifier_Node,
  * and back again.
@@ -18,66 +20,8 @@
 @interface HTMLPurifier_Arborize : NSObject
 
 
-- (NSArray*)arborizeTokens:(NSArray*)tokens config:(HTMLPurifier_Config*)config context:(HTMLPurifier_Context*)ceontext
-{
-        $definition = $config->getHTMLDefinition();
-        $parent = new HTMLPurifier_Token_Start($definition->info_parent);
-        $stack = array($parent->toNode());
-        foreach ($tokens as $token) {
-            $token->skip = null; // [MUT]
-            $token->carryover = null; // [MUT]
-            if ($token instanceof HTMLPurifier_Token_End) {
-                $token->start = null; // [MUT]
-                $r = array_pop($stack);
-                assert($r->name === $token->name);
-                assert(empty($token->attr));
-                $r->endCol = $token->col;
-                $r->endLine = $token->line;
-                $r->endArmor = $token->armor;
-                continue;
-            }
-            $node = $token->toNode();
-            $stack[count($stack)-1]->children[] = $node;
-            if ($token instanceof HTMLPurifier_Token_Start) {
-                $stack[] = $node;
-            }
-        }
-        assert(count($stack) == 1);
-        return $stack[0];
-    }
++ (NSArray*)arborizeTokens:(NSArray*)tokens config:(HTMLPurifier_Config*)config context:(HTMLPurifier_Context*)context;
 
-    public static function flatten($node, $config, $context) {
-        $level = 0;
-        $nodes = array($level => new HTMLPurifier_Queue(array($node)));
-        $closingTokens = array();
-        $tokens = array();
-        do {
-            while (!$nodes[$level]->isEmpty()) {
-                $node = $nodes[$level]->shift(); // FIFO
-                list($start, $end) = $node->toTokenPair();
-                if ($level > 0) {
-                    $tokens[] = $start;
-                }
-                if ($end !== NULL) {
-                    $closingTokens[$level][] = $end;
-                }
-                if ($node instanceof HTMLPurifier_Node_Element) {
-                    $level++;
-                    $nodes[$level] = new HTMLPurifier_Queue();
-                    foreach ($node->children as $childNode) {
-                        $nodes[$level]->push($childNode);
-                    }
-                }
-            }
-            $level--;
-            if ($level && isset($closingTokens[$level])) {
-                while ($token = array_pop($closingTokens[$level])) {
-                    $tokens[] = $token;
-                }
-            }
-        } while ($level > 0);
-        return $tokens;
-    }
-}
++ (NSArray*)flattenNode:(HTMLPurifier_Node*)node config:(HTMLPurifier_Config*)config context:(HTMLPurifier_Context*)context;
 
 @end
---
+++ HTMLPurifier/HTMLPurifier_Definition.m
@@ -0,0 +1,51 @@
+//
+//  HTMLPurifier_Definition.m
+//  HTMLPurifier
+//
+//  Created by Roman Priebe on 12.01.14.
+//  Copyright (c) 2014 Mynigma. All rights reserved.
+//
+
+#import "HTMLPurifier_Definition.h"
+#import "HTMLPurifier_Config.h"
+
+@implementation HTMLPurifier_Definition
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _setup = NO;
+        _optimized = NO;
+    }
+    return self;
+}
+
+    /**
+     * Sets up the definition object into the final form, something
+     * not done by the constructor
+     * @param HTMLPurifier_Config $config
+     */
+- (void)doSetup:(HTMLPurifier_Config*)config
+{
+
+}
+
+    /**
+     * Setup function that aborts if already setup
+     * @param HTMLPurifier_Config $config
+     */
+    - (void)setup:(HTMLPurifier_Config*)config;
+    {
+        if (_setup) {
+            return;
+        }
+        _setup = YES;
+        [self doSetup:config];
+    }
+
+
+
+
+@end
