80457854a6926eedb0219ea0e63db80abcb989a8
gitir1 <lksnmnn@gmail.com>
2014-01-13 16:11:27 +0100

    Added rest of attrDef Missing only some CSS

---
+++ HTMLPurifier/HTMLPurifier_AttrDef_Text.h
@@ -0,0 +1,13 @@
+//
+//  HTMLPurifier_AttrDef_Text.h
+//  HTMLPurifier
+//
+//  Created by Lukas Neumann on 13.01.14.
+//  Copyright (c) 2014 Mynigma. All rights reserved.
+//
+
+#import "HTMLPurifier_AttrDef.h"
+
+@interface HTMLPurifier_AttrDef_Text : HTMLPurifier_AttrDef
+
+@end
---
+++ HTMLPurifier/HTMLPurifier_AttrDef_URI.h
@@ -0,0 +1,13 @@
+//
+//  HTMLPurifier_AttrDef_URI.h
+//  HTMLPurifier
+//
+//  Created by Lukas Neumann on 13.01.14.
+//  Copyright (c) 2014 Mynigma. All rights reserved.
+//
+
+#import "HTMLPurifier_AttrDef.h"
+
+@interface HTMLPurifier_AttrDef_URI : HTMLPurifier_AttrDef
+
+@end
