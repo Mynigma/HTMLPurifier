c1f7e126b8d9eca2eed74e085c4367eed88f22d9
Roman Priebe <roman.priebe@gmail.com>
2014-01-15 18:07:36 +0100

    Unit tests compiling

--- HTMLPurifierTests/HTMLPurifier_Harness.m
+++ HTMLPurifierTests/HTMLPurifier_Harness.m
@@ -7,15 +7,13 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "HTMLPurifier_Harness.h"
 #import "HTMLPurifier_Config.h"
 #import "HTMLPurifier_Context.h"
 #import "HTMLPurifier_ConfigSchema.h"
 #import "HTMLPurifier.h"
 #import "BasicPHP.h"
 
-@interface HTMLPurifier_Harness : XCTestCase
-
-@end
 
 static HTMLPurifier_Config* config;
 static HTMLPurifier_Context* context;
